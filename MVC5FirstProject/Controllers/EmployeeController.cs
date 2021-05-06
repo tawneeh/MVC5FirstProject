7415ed58c0b372334b28ab5330e16bda2459d069
Tawnee Harris <tawneeh@icloud.com>
2021-05-05 17:35:47 -0700

    add EmployeeInfo

---
+++ MVC5FirstProject/Controllers/EmployeeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC5FirstProject.Models;
+
+namespace MVC5FirstProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult EmployeeInfo()
+        {
+            Employee employee = new Employee()
+            {
+                EmployeeId = 001,
+                EmployeeName = "Tawnee Harris",
+                EmployeeLocation = "Seattle"
+            };
+
+            return View();
+        }
+    }
+}
